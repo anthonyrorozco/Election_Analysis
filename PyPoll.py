e8ca48294eed3e613bfe99951d1a8083b9b33198
Anthony Orozco <anthonyrorozo119@gmail.com>
2022-06-27 20:14:49 -0500

    refactored the code

--- PyPoll.py
+++ PyPoll.py
@@ -16,94 +16,65 @@ now = dt.datetime.now()
 print("The time right now is ", now)
 
 # ---------------
+# Add our dependencies.
 import csv
-
 import os
-
-# Assign a variable for the file to load and the path.
+# Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
-
-# 1. Initialize a total vote counter.
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+# Initialize a total vote counter.
 total_votes = 0
-
-# Candidate Options
+# Candidate options and candidate votes
 candidate_options = []
-
-# Declare the empty dictionary.
 candidate_votes = {}
-
-# Open the election results and read the file.
-
-# Winning Candidate and Winning Count Tracker
+# Track the winning candidate, vote count, and percentage.
 winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
-
+# Open the election results and read the file.
 with open(file_to_load) as election_data:
-
-
-    # Read the file object with the render function.
     file_reader = csv.reader(election_data)
-
-
     # Read the header row.
     headers = next(file_reader)
-        
-        
-    #print each row in the CSV file.
+    # Print each row in the CSV file.
     for row in file_reader:
-        #2. Addd to the total vote count.
+        # Add to the total vote count.
         total_votes += 1
-
-    # Print the canidate name from each row
+        # Get the candidate name from each row.
         candidate_name = row[2]
-
-     # If the candidate does not match any existing candiddate...
+        # If the candidate does not match any existing candidate add it the
+        # the candidate list.
         if candidate_name not in candidate_options:
-
             # Add the candidate name to the candidate list.
             candidate_options.append(candidate_name)
-
-            # Begin tracking that candidate's vote count.
+            # And begin tracking that candidate's voter count.
             candidate_votes[candidate_name] = 0
-
-        # Add a vote to the candidate's count.
+        # Add a vote to that candidate's count
         candidate_votes[candidate_name] += 1
 
-# Detrmine the percentage of vote for each candidate by looping through the counts.
-# 1. Iterate through the candidate list.
 for candidate_name in candidate_votes:
-    # 2. Retrieve vote count of a candidate.
+    # Retrieve vote count and percentage.
     votes = candidate_votes[candidate_name]
-    # 3. Calculate the percentage of votes.
     vote_percentage = float(votes) / float(total_votes) * 100
-    # 4. Print the candidate name and percentage of votes.
-    
+    # Print each candidate, their voter count, and percentage to the
+    # terminal.
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
-    # Determine winning vote count and candidate
-    # 1. Determine if the votes are greater than the winning count.
+    # Determine winning vote count, winning percentage, and candidate.
     if (votes > winning_count) and (vote_percentage > winning_percentage):
-        # 2. if true then set winning_count = votes and wining_percent =
         winning_count = votes
-        winning_percentage = vote_percentage
-        # 3. Set the winning_candidate equal to the candidate's name.
         winning_candidate = candidate_name
-        print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
-
-
-
-    
-# to do: print out each candidate's name, vote count, and percentage of
-# votes to the terminal.
+        winning_percentage = vote_percentage
+# Print the winning candidates' results to the terminal.
 winning_candidate_summary = (
     f"-------------------------\n"
     f"Winner: {winning_candidate}\n"
     f"Winning Vote Count: {winning_count:,}\n"
     f"Winning Percentage: {winning_percentage:.1f}%\n"
     f"-------------------------\n")
+
 print(winning_candidate_summary)
-
-
 
 
 
